c0248b5595a73af7b29ac6919d90ba4f84f619ce
Burcu Telli <burcutelli35@gmail.com>
2025-09-11 13:21:17 +0000

    Operational cost added

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -5,7 +5,10 @@ ROUND((o.margin-s.shipping_fee-s.logcost-s.ship_cost),2) AS opeoperational_margi
 o.revenue,
 o.quantity,
 o.purchase_cost,
-o.margin
+o.margin,
+s.shipping_fee,
+s.logcost,
+s.ship_cost
 FROM {{ref("int_orders_margin")}} AS o
 LEFT JOIN {{ref("stg_raw__ship")}} AS s  
 ON o.orders_id=s.orders_id
